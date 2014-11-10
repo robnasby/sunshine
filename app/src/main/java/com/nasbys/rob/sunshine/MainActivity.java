ab44c668b8c6bd2b3b2ef8ce5895bb8c0413c699
Rob Nasby <rob.nasby@mcmaster.com>
2014-11-10 15:33:48 -0600

    Create the layout for the forecast list.

--- app/src/main/java/com/nasbys/rob/sunshine/MainActivity.java
+++ app/src/main/java/com/nasbys/rob/sunshine/MainActivity.java
@@ -59,7 +59,7 @@ public class MainActivity extends ActionBarActivity {
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            View rootView = inflater.inflate(R.layout.forecast_listview, container, false);
             return rootView;
         }
     }
