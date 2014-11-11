009809122b621e30ad355b5a5328c62321da12eb
Rob Nasby <rob.nasby@mcmaster.com>
2014-11-11 10:26:25 -0600

    Add forecast refresh menu item.

--- app/src/main/java/com/nasbys/rob/sunshine/ForecastFragment.java
+++ app/src/main/java/com/nasbys/rob/sunshine/ForecastFragment.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -27,6 +30,18 @@ public class ForecastFragment extends Fragment {
     private ArrayAdapter<String> _forecastAdapter;
 
     public ForecastFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        inflater.inflate(R.menu.menu_forecast, menu);
     }
 
     @Override
@@ -57,6 +72,22 @@ public class ForecastFragment extends Fragment {
         forecastListView.setAdapter(_forecastAdapter);
 
         return rootView;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_refresh) {
+            new FetchForecastDataTask().execute();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     public class FetchForecastDataTask extends AsyncTask<Void, Void, Void> {
