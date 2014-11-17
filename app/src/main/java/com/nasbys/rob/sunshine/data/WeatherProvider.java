0b7c502d730b1bec857dec2338f82f6291401a91
Rob Nasby <rob.nasby@mcmaster.com>
2014-11-17 10:07:33 -0600

    Instantiate a URI matcher for the weather provider.

--- app/src/main/java/com/nasbys/rob/sunshine/data/WeatherProvider.java
+++ app/src/main/java/com/nasbys/rob/sunshine/data/WeatherProvider.java
@@ -2,6 +2,7 @@ package com.nasbys.rob.sunshine.data;
 
 import android.content.ContentProvider;
 import android.content.ContentValues;
+import android.content.UriMatcher;
 import android.database.Cursor;
 import android.net.Uri;
 
@@ -9,6 +10,13 @@ import android.net.Uri;
  * Created by robnasby on 11/14/14.
  */
 public class WeatherProvider extends ContentProvider {
+    private static final int WEATHER = 100;
+    private static final int WEATHER_WITH_LOCATION = 101;
+    private static final int WEATHER_WITH_LOCATION_AND_DATE = 102;
+    private static final int LOCATION = 300;
+    private static final int LOCATION_ID = 301;
+
+    private static final UriMatcher _uriMatcher = buildUriMatcher();
     private static WeatherDbHelper _dbHelper;
 
     @Override
@@ -38,5 +46,21 @@ public class WeatherProvider extends ContentProvider {
     @Override
     public int update(Uri uri, ContentValues contentValues, String s, String[] strings) {
         return 0;
+    }
+
+    private static UriMatcher buildUriMatcher() {
+        UriMatcher matcher = new UriMatcher(UriMatcher.NO_MATCH);
+
+        final String authority = WeatherContract.CONTENT_AUTHORITY;
+        final String locationBasePath = WeatherContract.PATH_LOCATION;
+        final String weatherBasePath = WeatherContract.PATH_WEATHER;
+
+        matcher.addURI(authority, weatherBasePath, WEATHER);
+        matcher.addURI(authority, weatherBasePath + "/*", WEATHER_WITH_LOCATION);
+        matcher.addURI(authority, weatherBasePath + "/*/*", WEATHER_WITH_LOCATION_AND_DATE);
+        matcher.addURI(authority, locationBasePath, LOCATION);
+        matcher.addURI(authority, locationBasePath + "/#", LOCATION_ID);
+
+        return matcher;
     }
 }
